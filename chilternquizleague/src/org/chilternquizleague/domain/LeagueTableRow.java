9ccd72cad0e9246b80ef204028c6978f44a7025b
gumdrop <davidcgood@gmail.com>
2014-06-12 21:02:24 +0100

    Add file missing from last commit

---
+++ chilternquizleague/src/org/chilternquizleague/domain/LeagueTableRow.java
@@ -0,0 +1,63 @@
+package org.chilternquizleague.domain;
+
+public class LeagueTableRow {
+	
+	private String team;
+	private int played;
+	private int won;
+	private int lost;
+	private int drawn;
+	private int leaguePoints;
+	private int matchPointsFor;
+	private int matchPointsAgainst;
+	
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public int getPlayed() {
+		return played;
+	}
+	public void setPlayed(int played) {
+		this.played = played;
+	}
+	public int getWon() {
+		return won;
+	}
+	public void setWon(int won) {
+		this.won = won;
+	}
+	public int getLost() {
+		return lost;
+	}
+	public void setLost(int lost) {
+		this.lost = lost;
+	}
+	public int getDrawn() {
+		return drawn;
+	}
+	public void setDrawn(int drawn) {
+		this.drawn = drawn;
+	}
+	public int getLeaguePoints() {
+		return leaguePoints;
+	}
+	public void setLeaguePoints(int matchPoints) {
+		this.leaguePoints = matchPoints;
+	}
+	public int getMatchPointsFor() {
+		return matchPointsFor;
+	}
+	public void setMatchPointsFor(int leaguePointsFor) {
+		this.matchPointsFor = leaguePointsFor;
+	}
+	public int getMatchPointsAgainst() {
+		return matchPointsAgainst;
+	}
+	public void setMatchPointsAgainst(int leaguePointsAgainst) {
+		this.matchPointsAgainst = leaguePointsAgainst;
+	}
+
+}
