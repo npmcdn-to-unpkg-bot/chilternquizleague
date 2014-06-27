59b0c159923c09870721199215ca8676965268f6
dgood <davidcgood@gmail.com>
2014-06-27 15:33:24 +0100

    Started on teams

--- chilternquizleague/WebContent/js/controller.js
+++ chilternquizleague/WebContent/js/controller.js
@@ -1,36 +1,50 @@
-var qlApp = angular.module('qlApp', []);
+var qlApp = angular.module('qlApp', [ "ngRoute" ]).factory('entityService',
+		ENTITY_SERVICE_DEFN);
 
-qlApp.controller('ResultsController', [ '$scope', '$http', '$interval',
+qlApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/team/:teamId', {
+		templateUrl : 'team/teams.html',
+		controller : 'GlobalDetailCtrl'
+	}).otherwise({
+		redirectTo : ''
+	});
+} ]);
+
+qlApp.controller('TeamsController', [
+		'$scope',
+		'$http',
+		'$interval',
 		function($scope, $http, $interval) {
 
 			$http.get("jaxrs/globaldata", {
 				"responseType" : "json"
-			}).success(function(globalData){
-				
-				$scope.leagueName = globalData.leagueName;
-				$scope.frontPageText=globalData.frontPageText;
-				
-				var promise = $interval(function() {
+			}).success(
+					function(globalData) {
 
-					$http.get("jaxrs/leaguetable/" + globalData.currentSeasonId, {
-						"responseType" : "json"
-					}).success(function(ret) {
-						$scope.season = ret;
-					}).error(function(){$interval.cancel(promise)});
-				}, 1000, 120);
-			});
-	
-			
-			
-			
+						$scope.leagueName = globalData.leagueName;
+						$scope.frontPageText = globalData.frontPageText;
+
+						var promise = $interval(function() {
+
+							$http.get(
+									"jaxrs/leaguetable/"
+											+ globalData.currentSeasonId, {
+										"responseType" : "json"
+									}).success(function(ret) {
+								$scope.season = ret;
+							}).error(function() {
+								$interval.cancel(promise)
+							});
+						}, 1000, 120);
+					});
+
 		} ]);
 
-qlApp.controller('TeamsController', [ '$scope', '$http', '$interval','entityService',
-                                		function($scope, $http, $interval, entityService) {
-	
-	
-		entityService.getList("team",function(teams){$scope.teams = teams;});
-	
-	
-	
-}]);
+qlApp.controller('TeamsController', [ '$scope', '$http', '$interval',
+		'entityService', function($scope, $http, $interval, entityService) {
+
+			entityService.getList("team", function(teams) {
+				$scope.teams = teams;
+			});
+
+		} ]);
