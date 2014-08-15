db9cd991cf4ca67bc9513f760b1c3085038e5c08
gumdrop <davidcgood@gmail.com>
2014-08-15 21:33:58 +0100

    Email works!

--- chilternquizleague/src/org/chilternquizleague/web/MailHandlerServlet.java
+++ chilternquizleague/src/org/chilternquizleague/web/MailHandlerServlet.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.mail.Address;
 import javax.mail.MessagingException;
@@ -26,87 +28,91 @@ import org.chilternquizleague.domain.User;
 
 import com.googlecode.objectify.Key;
 
-
-
 @SuppressWarnings("serial")
-public class MailHandlerServlet extends HttpServlet { 
-    
-	
-	
+public class MailHandlerServlet extends HttpServlet {
+
+	private final static Logger LOG = Logger.getLogger(MailHandlerServlet.class
+			.getName());
+
 	@Override
-    public void doPost(HttpServletRequest req, 
-                       HttpServletResponse resp) 
-            throws IOException { 
-        Properties props = new Properties(); 
-        Session session = Session.getDefaultInstance(props, null); 
-        try {
-			final MimeMessage message = new MimeMessage(session, req.getInputStream());
-		
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		try {
+			final MimeMessage message = new MimeMessage(session,
+					req.getInputStream());
+
 			final GlobalApplicationData globaldata = ofy().load().now(
 					Key.create(GlobalApplicationData.class,
 							AppStartListener.globalApplicationDataId));
-			
-			final String localHost = new URI(req.getRequestURI()).getHost();
 
-			for(Address recip : message.getAllRecipients()){
+
+
+			for (Address recip : message.getAllRecipients()) {
 
 				final String recipientName = recip.toString().split("@")[0];
-			
-				for(EmailAlias alias:globaldata.getEmailAliases()){
-				
-				if(alias.getAlias().equals(recipientName)){
-					sendMail(message, localHost, new MimeMessage(session), new InternetAddress(alias.getUser().getEmail()));					
-					return;
-				}
-			}
-			
-			
-			final List<Team> teams = ofy().load().type(Team.class).list();
-			
-			for(Team team : teams){
-				
-					
-					
-					if(recipientName.equals(team.getEmailName())){
-						
-					MimeMessage  outMessage = new MimeMessage(session);
-					
-					final Address[] addresses = new Address[team.getUsers().size()];
-					
+				final String recipientHost = recip.toString().split("@")[1];
 
-					int idx  = 0;
-					for(User user : team.getUsers()){
-						addresses[idx++] = new InternetAddress(user.getEmail());
-					}
-					
-					sendMail(message, localHost, outMessage, addresses);
-					
-					return;
+				for (EmailAlias alias : globaldata.getEmailAliases()) {
+
+					if (alias.getAlias().equals(recipientName)) {
+						sendMail(message, recipientHost, new MimeMessage(session),
+								new InternetAddress(alias.getUser().getEmail()));
+						return;
 					}
 				}
-				
-				
+
+				final List<Team> teams = ofy().load().type(Team.class).list();
+
+				for (Team team : teams) {
+
+					if (recipientName.equals(team.getEmailName())) {
+
+						MimeMessage outMessage = new MimeMessage(session);
+
+						final Address[] addresses = new Address[team.getUsers()
+								.size()];
+
+						int idx = 0;
+						for (User user : team.getUsers()) {
+							addresses[idx++] = new InternetAddress(
+									user.getEmail());
+						}
+
+						sendMail(message, recipientHost, outMessage, addresses);
+
+						return;
+					}
+				}
+
 			}
 			
-			
-        
-        } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOG.fine("No matching addressees for any recipients");
+
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Failure recieving mail", e);
 		}
-    }
+	}
 
 	private void sendMail(final MimeMessage message, final String localHost,
 			final MimeMessage outMessage, final Address... addresses)
 			throws MessagingException, AddressException, IOException {
-		outMessage.addRecipients(RecipientType.TO, addresses);
-		outMessage.setSender(new InternetAddress("forwarding@"+ localHost));
-		outMessage.setContent(message.getContent(), message.getContentType());
-		outMessage.setSubject(message.getSubject());
-		
-		System.out.println(outMessage.getFrom()[0] + " to " + outMessage.getAllRecipients()[0].toString());
+		try {
 
-		
-		Transport.send(outMessage);
+			outMessage.addRecipients(RecipientType.TO, addresses);
+			outMessage
+					.setSender(new InternetAddress("forwarding@" + localHost));
+			outMessage.setContent(message.getContent(),
+					message.getContentType());
+			outMessage.setSubject(message.getSubject());
+
+			LOG.fine(outMessage.getFrom()[0] + " to "
+					+ outMessage.getAllRecipients()[0].toString());
+
+			Transport.send(outMessage);
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Failure sending mail", e);
+		}
 	}
 }
