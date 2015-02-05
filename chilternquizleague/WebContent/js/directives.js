11900f77462af955e0baf431dfaea833c44db479
dgood <davidcgood@gmail.com>
2015-02-05 10:08:08 +0000

    menu fixes and rationalisation

--- chilternquizleague/WebContent/js/directives.js
+++ chilternquizleague/WebContent/js/directives.js
@@ -160,15 +160,17 @@ mainApp.directive("cqlSubheader", function(){
 	}
 })
 
-mainApp.directive("cqlPageNav", function(){
+mainApp.directive("cqlPageNav",["$mdSidenav", function($mdSidenav){
 
 	return {
 		scope:{"title":"@"},
 		restrict:'E',
 		transclude:true,
 		link: function(scope){
+			scope.closeLeft = function(){
+				$mdSidenav('left').close();};
 			scope.classes = "md-subheader md-default-theme"
 		},
 		templateUrl: "/common/sidenav.html"
 	}
-})
+}])
