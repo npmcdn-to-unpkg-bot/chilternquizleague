ae4bc27075138e8796bae853f997f1718fd360bc
gumdrop <davidcgood@gmail.com>
2016-04-16 16:15:38 +0100

    Fixed index page content

--- WebContent/js/directives.js
+++ WebContent/js/directives.js
@@ -131,7 +131,7 @@ mainApp.directive('cqlSeasons', ["viewService","seasonService","$rootScope",func
 	  			scope.seasons = seasons})
     		scope.hidelabel = attrs.hasOwnProperty("hidelabel");
     		scope.selectstyle = attrs.hasOwnProperty("toolstyle") ? {background:"inherit",border:"none",color:"inherit"}:{};
-    		scope.containerstyle = attrs.hasOwnProperty("toolstyle") ? {padding:"0",paddingBottom:".45em",paddingLeft:".25em"}:{}
+    		scope.containerstyle = attrs.hasOwnProperty("toolstyle") ? {padding:"0",paddingBottom:".2em",paddingLeft:".25em"}:{}
     		scope.toolclass = attrs.hasOwnProperty("toolstyle") ?"tool" : ""
     		if(attrs.hasOwnProperty("broadcast")){
     			scope.$watch("season", function(season){$rootScope.$broadcast("season",season)})
