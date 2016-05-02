320a351f36ad2f2729eeb94e974c36dbbdb44b2e
gumdrop <davidcgood@gmail.com>
2016-05-02 20:22:07 +0100

    made sponsor message configurable from text

--- src/org/chilternquizleague/domain/Entities.scala
+++ src/org/chilternquizleague/domain/Entities.scala
@@ -128,7 +128,6 @@ class GlobalApplicationData extends BaseEntity{
   var frontPageText:String = null
   var leagueName:String = null
   var senderEmail:String = null
-  var sponsorMessage:String = null
 
   @Load
   var currentSeason:Ref[Season] = null
